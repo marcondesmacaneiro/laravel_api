218ae45dd3b340204b6e0cf3547dd35d8b7db005
William Tiago Velho da Silva Goebel <tiagowtvs@hotmail.com>
2018-10-16 21:32:35 -0300

    release 2.0

---
+++ laravel/src/resources/views/ViewAdicionaProdutosWilliam.blade.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Modelo dos Clientes
+ *
+ * @package Model
+ * @author  William Goebel
+ * @since   02/10/2018
+ */
+
+
+?>
+
+@extends('ViewPadrao')
+
+@section('content')
+
+
+    <form>
+       
+    </form>       
+                           
+
+@endsection
+
+@section('scripts')
+<script type="text/javascript" src="js/Produtos.js"></script>
+@endsection
